7a2eaddba80d5d7836ffb26b6b5087db7035838b
José Eduardo <dev.souzaedu@gmail.com>
2024-05-03 13:52:33 -0300

    v.5.0

---
+++ submit_contact_form.php
@@ -0,0 +1,18 @@
+<?php
+$nome_cliente = $_POST['name'];
+$nome_empresa = $_POST['company'];
+$mensagem = $_POST['message'];
+
+$assunto = 'Contato do Site - ' . $nome_cliente;
+$remetente = 'contato@seusite.com';
+$destinatario = 'dev.souzaedu@gmail.com'; // Substitua pelo seu endereço de e-mail
+$cabecalho = "From: $remetente\r\nContent-type: text/plain; charset=utf-8";
+
+// Verifique se o formulário foi enviado corretamente
+if (!empty($nome_cliente) && !empty($nome_empresa) && !empty($mensagem)) {
+    mail($destinatario, $assunto, $mensagem, $cabecalho);
+    echo 'Mensagem enviada com sucesso!';
+} else {
+    echo 'Por favor, preencha todos os campos.';
+}
+?>
